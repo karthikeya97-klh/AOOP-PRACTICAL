97876f191bb6eca218e5eab55c817f6bd57bbd1c
karthikeya97-klh <2320030097@klh.edu.in>
2024-09-14 13:39:44 +0530

    Delete WEEK 6 INLAB directory

--- WEEK 6 INLAB/src/EnrollmentService.java
+++
@@ -1,9 +0,0 @@
-package student;
-
-
-public class EnrollmentService {
-    public void enroll(Student student, Course course) {
-        student.enrollCourse(course);
-        course.enrollStudent(student);
-    }
-}
